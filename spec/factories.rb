a85c4ec59b0419a768b5496a5b47a47176539ba0
larkinalan <larkinalan@gmail.com>
2010-11-10 23:31:57 +0000

    added factories and tests for show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+    user.name                   "Alan Larkin"
+    user.email                  "alanl@example.com"
+    user.password               "foobar"
+    user.password_confirmation  "foobar"
+end
