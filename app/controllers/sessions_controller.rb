6e2a63ead625170c43f2f6f54ba8f4ba1e46ef17
larkinalan <larkinalan@gmail.com>
2010-11-28 15:30:48 +0000

    Working user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
        @title = "Sign in"
        render 'new'
     else
-      # Handle succesful sigin
+      sign_in user
+      redirect_to user
     end
   end
   
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
     user.name                   "Alan Larkin"
-    user.email                  "alanl@example.com"
+    user.email                  "alanlarkin@example.com"
     user.password               "foobar"
     user.password_confirmation  "foobar"
 end
